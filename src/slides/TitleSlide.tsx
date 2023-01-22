e880c06f6fe08a47a939f7bd93fc147e0c1bdbc2
Maxime BAUMANN <499268+mbaumanndev@users.noreply.github.com>
2023-01-22 20:32:52 +0100

    notes

--- src/slides/TitleSlide.tsx
+++ src/slides/TitleSlide.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { Slide } from "../components/Reveal";
+import { Notes, Slide } from "../components/Reveal";
 
 export default function TitleSlide() {
     return <Slide>
         <h2>Séparer les responsabilités</h2>
         <h4>Architecture d'une application React</h4>
         <h6>Maxime Baumann</h6>
+        <Notes>
+            - Pistes de réflexions
+        </Notes>
     </Slide>
 }
